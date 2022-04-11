ce0b56e73e080710725978a0034c7e590b13171b
AlbertoV5 <avq5ac1@gmail.com>
2022-04-10 22:10:07 -0500

    added "Channel" page for Realearn static name control
    You need to give an alias to the parameter to control it as well as setting the plugin name to "Channel". Aliases are: ["LPF", "HPF", "1DB", "1FQ", "2DB", "2FQ", "3DB", "3FQ", "4DB", "4FQ", "CTR", "CRA", "CAT", "CRE", "CMO"]
    They are intended for the average channel strip plugin

---
+++ scripts/realearn-python/generate2.py
@@ -0,0 +1,39 @@
+# 1. Create a mapping in Realearn
+# 2. Right click, copy and paste in mapping.json
+# 3. Tweak this script, run it. Paste output.json contents in Realearn
+from pathlib import Path
+from uuid import uuid4
+import json
+
+directory = Path.cwd()
+directory = directory / "scripts/realearn-python"
+filePath = directory / "mapping.json"
+
+jsonData = {"kind": "Mappings", "value":[]}
+
+aliases = ["LPF", "HPF", "1DB", "1FQ", "2DB", "2FQ", "3DB", "3FQ", "4DB", "4FQ", "CTR", "CRA", "CAT", "CRE", "CMO"]
+
+control = "value"
+
+trackNum = "2"
+expression = "selected_track_index + (p[19] * 1000)"
+
+for i in range(len(aliases)):
+
+    with open(filePath, "r") as file:
+        jsm = json.loads(file.read())["value"]
+
+    jsm["id"] = str(uuid4())
+    jsm["name"] = aliases[i] + " " + control
+    jsm["source"]["oscAddressPattern"] = "/channel/track/" + str(trackNum) + "/fxparam/" + str(i+1) + "/" + control
+
+    jsm["target"]["trackExpression"] = expression
+    jsm["target"]["fxName"] = "Channel"
+    jsm["target"]["paramName"] = aliases[i]
+
+    jsonData["value"].append(jsm)
+
+
+with open (directory / "output.json", "w") as file:
+    file.write(json.dumps(jsonData, indent=2))
+
