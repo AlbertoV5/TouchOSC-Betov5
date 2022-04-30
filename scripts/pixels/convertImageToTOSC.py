699041a37b77438177fdc83c01c6fe22a281773c
AlbertoV5 <avq5ac1@gmail.com>
2022-04-29 23:53:12 -0500

    adding tkinter

--- scripts/pixels/convertImageToTOSC.py
+++ scripts/pixels/convertImageToTOSC.py
@@ -1,4 +1,14 @@
 import toscEdit
+from tkinter import Tk, ttk
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
+
+# TOSC
 
 tosc = toscEdit.Design(directory = "scripts/pixels")
 
